a30217f4aa9e5603850f8802572652b4928a2059
danielmishan85 <danielmishan85@cs.colman.ac.il>
2022-06-08 18:55:14 +0300

    chain multiple middleware function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,20 +5,34 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
-    next();
   }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
 };
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
-    result: tours.length,
+    requestedAt: req.requestTime,
+    results: tours.length,
     data: {
       tours,
     },
@@ -30,7 +44,8 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
-  status(200).json({
+
+  res.status(200).json({
     status: 'success',
     data: {
       tour,
@@ -39,8 +54,11 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
   tours.push(newTour);
 
   fs.writeFile(
